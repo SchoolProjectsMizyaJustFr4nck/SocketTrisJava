1fa532e4f057e045345aa911215a22d883102daf
lai2-24 <lai2-24@itismeucci.com>
2025-11-04 13:05:13 +0100

    server release beta 0.1.0

--- server/src/main/java/com/server/Main.java
+++ server/src/main/java/com/server/Main.java
@@ -8,74 +8,156 @@ import java.net.ServerSocket;
 import java.net.Socket;
 //Server
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    static int [] gameBoard = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    static int pos = 0;
+    static int p1 = 1;
+    static int p2 = 2;
 
-        System.out.println("Server socket in ascolto sulla porta 3000");
-        ServerSocket ss = new ServerSocket(3000);   //apro server socket sulla porta 3000
-        Socket myS = ss.accept();                        //crea un socket in ascolta da server, accept blocca, finche qualcuno non si collega alla porta 3000
-        System.out.println("Si è collegato un utente con ip: " + myS.getInetAddress());
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(myS.getInputStream()));
-        PrintWriter out = new PrintWriter(myS.getOutputStream(), true);
-        
-        out.println("Welcome to server v2.1");
-        int i = 0;
-
-        int row = -1;
-        int col = -1;
-        String move = "";
-
-        do{
+    static int[][] winCombinations = {
+            {0, 1, 2}, // prima riga
+            {3, 4, 5}, // seconda riga
+            {6, 7, 8}, // terza riga
+            {0, 3, 6}, // prima colonna
+            {1, 4, 7}, // seconda colonna
+            {2, 5, 8}, // terza colonna
+            {0, 4, 8}, // diagonale principale
+            {2, 4, 6}  // diagonale secondaria
+        };
+    
+        public static void main(String[] args) throws IOException {
+            
+            System.out.println("Server socket in ascolto sulla porta 3000");
+            ServerSocket ss = new ServerSocket(3000);   //apro server socket sulla porta 3000
+            
+            //UTENTE 1
+            Socket myS1 = ss.accept();                        //crea un socket in ascolta da server, accept blocca, finche qualcuno non si collega alla porta 3000
+            System.out.println("Si è collegato il primo utente con ip: " + myS1.getInetAddress());
+            BufferedReader in1 = new BufferedReader(new InputStreamReader(myS1.getInputStream()));
+            PrintWriter out1 = new PrintWriter(myS1.getOutputStream(), true);
+            out1.println("WAIT");
+    
+            //UTENTE 2
+            Socket myS2 = ss.accept();
+            System.out.println("Si è collegato il primo utente con ip: " + myS2.getInetAddress());
+            BufferedReader in2 = new BufferedReader(new InputStreamReader(myS2.getInputStream()));
+            PrintWriter out2 = new PrintWriter(myS2.getOutputStream(), true);
+            out1.println("READY");
+            out2.println("READY");
 
             while(true){
 
                 do{
-
-                    row = Integer.parseInt(in.readLine());
-                    if(checkPos(row)){
-
-                        out.println("ERROR: WRITE A EXISTED LINE");
+                    pos = Integer.parseInt(in1.readLine()) - 1;
+                    if(!testInputPos(pos) || checkIfExist(pos)){
+                        out1.println("KO");
+                    } else if(winCondition(gameBoard, p1)){
+                        out1.println("W");
+                        out2.println(printArr(gameBoard) + ", L");
+                        ss.close(); 
+                        return;
                     }
-                    
-                }while(checkPos(row));
-
+                    else if(checkTie(gameBoard)){
+                        out1.println("P");
+                        out2.println("P");
+                        ss.close();
+                        return;
+                    }
+                    else{
+                        out1.println("OK");
+                        out2.println(printArr(gameBoard));
+                    }
+                }while(testInputPos(pos));
+    
                 do{
-
-                    col = Integer.parseInt(in.readLine());
-                    if(checkPos(col)){
-
-                        out.println("ERROR: WRITE A EXISTED COLUMN");
+                    pos = Integer.parseInt(in2.readLine()) - 1;
+                    if(!testInputPos(pos) || checkIfExist(pos)){
+                        out2.println("KO");
+                    } else if(winCondition(gameBoard, p2)){
+                        out2.println("W");
+                        out1.println(printArr(gameBoard) + ", L");
+                        ss.close(); 
+                        return;
                     }
-                    
-                }while(checkPos(col));
-
-                
-
-                i++;
-                break;
+                    else if(checkTie(gameBoard)){
+                        out1.println("P");
+                        out2.println("P");
+                        ss.close();
+                        return;
+                    }
+                    else{
+                        out2.println("OK");
+                        out1.println(printArr(gameBoard));
+                    }
+                }while(testInputPos(pos));
             }
+    
             
             
-
-
-
             
-        }while(i < 9);
-        ss.close();  //chiudo la porta
+            
+            
+             //chiudo la porta
+            
+        }
+    
+    
+        static public Boolean testInputPos(int pos){
+            
+            if(pos < 0 || pos >= 9){
+    
+                return false;
+            }
+            return true;
+        }
+    
+        static public Boolean checkIfExist(int pos){
+    
+            if(gameBoard[pos] != 0){
+    
+                return false;
+            }
+          
+            return true;
+        }
+    
+        static public String printArr(int gameBoard[]){
+            StringBuilder s = new StringBuilder();
+    
+            for (int i = 0; i < gameBoard.length; i++) {
+                s.append(gameBoard[i]);  
+                if (i < gameBoard.length - 1) {
+                    s.append(", "); 
+                }
+            }
+    
+            return s.toString();
+        }
+    
+        static public Boolean winCondition(int gameBoard [], int p){
+            for (int[] combination : winCombinations) {
+                if (gameBoard[combination[0]] == p &&
+                gameBoard[combination[1]] == p &&
+                gameBoard[combination[2]] == p) {
+                return true; 
+                }
+            }
+        return false;
+        }
+
+        static public Boolean checkTie(int gameBoard []){
+            
+            for(int i = 0; i < gameBoard.length; i++){
+
+                if(gameBoard[i] == 0){
+
+                    return false;
+                }
+            }
+            return true;
+        }
         
     }
 
-
-
-    public static Boolean checkPos(int p){
-
-        if(p > 3 || p < 1){
-
-            return false;
-        }
-        return true;
-    }
-}
-
 //10.22.9.6
+
